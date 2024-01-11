9aea29577626c23780e4f96ddbda08a834889cd7
Adar <adar.sonmez@arena.com.tr>
2024-01-12 00:06:12 +0300

    Configure DbContext

---
+++ DataAccess/Concrete/EntityFramework/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class AppDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=NTierArchitecture;Integrated Security=True;");
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<UserOperationClaim>? UserOperationClaims { get; set; }
+        public DbSet<OperationClaim>? OperationClaims { get; set; }
+    }
+}
