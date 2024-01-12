7cfd7f9fb1449c6a714f7edb59e5819112f1e3e2
Adar <adar.sonmez@arena.com.tr>
2024-01-12 12:56:06 +0300

    Configure Autofac dependency injection module

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,11 +1,24 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.Abstract;
+using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IOperationClaimService, OperationClaimManager>();
+builder.Services.AddScoped<IOperationClaimDal, EfOperationClaimDal>();
 
 var app = builder.Build();
 
